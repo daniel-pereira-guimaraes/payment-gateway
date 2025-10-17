df7b2c9bc8d81d237eebbdd2d0cc8c382eae0df9
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-10-16 23:02:35 -0400

    Criada a classe ChargeId e testes

---
+++ src/main/java/com/danielpg/paymentgateway/domain/charge/ChargeId.java
@@ -0,0 +1,21 @@
+package com.danielpg.paymentgateway.domain.charge;
+
+import com.danielpg.paymentgateway.domain.SurrogateId;
+
+import java.util.Optional;
+
+public class ChargeId extends SurrogateId {
+
+    protected ChargeId(Long value) {
+        super(value);
+    }
+
+    public static ChargeId of(Long value) {
+        return new ChargeId(value);
+    }
+
+    public static Optional<ChargeId> ofNullable(Long value) {
+        return value == null ? Optional.empty()
+                : Optional.of(new ChargeId(value));
+    }
+}
