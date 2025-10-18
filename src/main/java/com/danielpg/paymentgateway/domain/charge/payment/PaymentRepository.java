8bdb15208b3c4c5c831f3274ae977b86647b411e
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-10-17 21:57:00 -0400

    Adicionado método get por PaymentId na interface PaymentRepository

--- src/main/java/com/danielpg/paymentgateway/domain/charge/payment/PaymentRepository.java
+++ src/main/java/com/danielpg/paymentgateway/domain/charge/payment/PaymentRepository.java
@@ -3,7 +3,10 @@ package com.danielpg.paymentgateway.domain.charge.payment;
 
 import com.danielpg.paymentgateway.domain.charge.ChargeId;
 
+import java.util.Optional;
+
 public interface PaymentRepository {
-    boolean exists(ChargeId charId);
+    boolean exists(ChargeId chargeId);
+    Optional<Payment> get(PaymentId id);
     void save(Payment payment);
 }
