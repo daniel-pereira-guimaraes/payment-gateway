e9117ee95a7e12126d490c5695aba3022c7d4752
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-10-18 18:49:43 -0400

    Adicionada mensagem em PaymentNotAuthorizedException

--- src/main/java/com/danielpg/paymentgateway/domain/charge/payment/PaymentNotAuthorizedException.java
+++ src/main/java/com/danielpg/paymentgateway/domain/charge/payment/PaymentNotAuthorizedException.java
@@ -1,4 +1,8 @@
 package com.danielpg.paymentgateway.domain.charge.payment;
 
 public class PaymentNotAuthorizedException extends RuntimeException {
+
+    public PaymentNotAuthorizedException() {
+        super("Pagamento não autorizado.");
+    }
 }
