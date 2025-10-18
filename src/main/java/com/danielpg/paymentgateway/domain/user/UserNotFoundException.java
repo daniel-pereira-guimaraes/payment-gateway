97d99f8cd69ee052c73d8e50c5af5ccd05b2860b
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-10-18 12:07:30 -0400

    Adicionado construtor com argumento email em UserNotFoundException

--- src/main/java/com/danielpg/paymentgateway/domain/user/UserNotFoundException.java
+++ src/main/java/com/danielpg/paymentgateway/domain/user/UserNotFoundException.java
@@ -6,6 +6,7 @@ public class UserNotFoundException extends AbstractNotFoundException {
 
     public static final String USER_WITH_CPF_NOT_FOUND = "Usuário com CPF %s não encontrado.";
     public static final String USER_WITH_ID_NOT_FOUND = "Usuário com ID %d não encontrado.";
+    public static final String USER_WITH_EMAIL_NOT_FOUND = "Usuário com email %s não encontrado.";
 
     public UserNotFoundException(Cpf cpf) {
         super(USER_WITH_CPF_NOT_FOUND.formatted(cpf.value()));
@@ -13,5 +14,9 @@ public class UserNotFoundException extends AbstractNotFoundException {
 
     public UserNotFoundException(UserId id) {
         super(USER_WITH_ID_NOT_FOUND.formatted(id.value()));
+    }
+
+    public UserNotFoundException(EmailAddress emailAddress) {
+        super(USER_WITH_EMAIL_NOT_FOUND.formatted(emailAddress.value()));
     }
 }
