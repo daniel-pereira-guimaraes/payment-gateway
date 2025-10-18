92f82744f20759f0911958c5b08c97a934875dd1
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-10-18 12:10:33 -0400

    Implementação de UserDetails

---
+++ src/main/java/com/danielpg/paymentgateway/infrastructure/security/UserDetailsImpl.java
@@ -0,0 +1,46 @@
+package com.danielpg.paymentgateway.infrastructure.security;
+
+import com.danielpg.paymentgateway.domain.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public record UserDetailsImpl(User user) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.hashedPassword().hash();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.emailAddress().value();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
