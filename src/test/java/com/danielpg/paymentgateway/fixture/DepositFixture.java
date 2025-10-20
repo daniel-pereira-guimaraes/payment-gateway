0ee76a586aab988146b2fb85ea3ce166090b8324
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-10-20 14:18:50 -0400

    Limpeza - removido código não utilizado

--- src/test/java/com/danielpg/paymentgateway/fixture/DepositFixture.java
+++ src/test/java/com/danielpg/paymentgateway/fixture/DepositFixture.java
@@ -2,7 +2,6 @@ package com.danielpg.paymentgateway.fixture;
 
 import com.danielpg.paymentgateway.domain.deposit.Deposit;
 import com.danielpg.paymentgateway.domain.deposit.DepositId;
-import com.danielpg.paymentgateway.domain.deposit.DepositRequest;
 import com.danielpg.paymentgateway.domain.shared.PositiveMoney;
 import com.danielpg.paymentgateway.domain.shared.TimeMillis;
 import com.danielpg.paymentgateway.domain.user.UserId;
@@ -25,10 +24,6 @@ public class DepositFixture {
                 .withUserId(USER_ID)
                 .withAmount(AMOUNT)
                 .withCreatedAt(CREATED_AT);
-    }
-
-    public static DepositRequest aDepositRequest() {
-        return DepositRequest.of(USER_ID, AMOUNT);
     }
 
 }
