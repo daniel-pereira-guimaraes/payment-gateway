85eded5123a6a48a22d20a86ec41759699dbb88f
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-10-21 12:29:12 -0400

    Adicionado título, descrição e contato na documentação Swagger

--- src/main/java/com/danielpg/paymentgateway/infrastructure/configuration/swagger/OpenApiConfig.java
+++ src/main/java/com/danielpg/paymentgateway/infrastructure/configuration/swagger/OpenApiConfig.java
@@ -1,6 +1,8 @@
 package com.danielpg.paymentgateway.infrastructure.configuration.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
@@ -20,7 +22,22 @@ import org.springframework.context.annotation.Configuration;
                 @Tag(name = "04 - Cobranças", description = "Gerenciamento de cobranças."),
                 @Tag(name = "05 - Pagamentos", description = "Gerenciamento de pagamentos."),
                 @Tag(name = "06 - Monitoramento", description = "Monitoramento do estado do serviço.")
-        }
+        },
+        info = @Info(
+                title = "Payment Gateway API",
+                version = "1.0.0",
+                description = """
+                    API responsável pelo processamento de cobranças e pagamentos,
+                    desenvolvida com propósito didático e para demonstração de
+                    meus conhecimentos como desenvolvedor backend, usando Java,
+                    Spring Boot, JDBC, JUnit, Mockito e Swagger, aplicando boas
+                    práticas de TDD, DDD, Clean Code e Clean Architecture.
+                    """,
+                contact = @Contact(
+                        name = "Daniel Pereira Guimarães",
+                        url = "https://www.linkedin.com/in/danielpereiraguimaraes"
+                )
+        )
 )
 public class OpenApiConfig {
 
