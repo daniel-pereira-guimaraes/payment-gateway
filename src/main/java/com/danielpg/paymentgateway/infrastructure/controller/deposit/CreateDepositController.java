ee69b67ef95842b7ce593a8215ecb0dd3143a56e
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-10-21 07:09:51 -0400

    Ajustes na documentação do Swagger em CreateDepositController

--- src/main/java/com/danielpg/paymentgateway/infrastructure/controller/deposit/CreateDepositController.java
+++ src/main/java/com/danielpg/paymentgateway/infrastructure/controller/deposit/CreateDepositController.java
@@ -4,7 +4,10 @@ import com.danielpg.paymentgateway.application.deposit.CreateDepositUseCase;
 import com.danielpg.paymentgateway.domain.deposit.Deposit;
 import com.danielpg.paymentgateway.domain.shared.PositiveMoney;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
-@Tag(name = "Depósito", description = "Criação de depósito.")
+@Tag(name = "Depósito", description = "Criação de depósitos.")
 @RestController
 @RequestMapping("/deposits")
 public class CreateDepositController {
@@ -23,14 +26,15 @@ public class CreateDepositController {
     private CreateDepositUseCase useCase;
 
     @PostMapping
+    @SecurityRequirement(name = "bearerAuth")
     @Operation(
             summary = "Cria um depósito",
-            description = "Cria um depósito, cujo valor será somado ao saldo do usuário autenticado",
+            description = "Cria um depósito, cujo valor será somado ao saldo do usuário autenticado.",
             responses = {
-                    @ApiResponse(responseCode = "201", description = "Depósito criado com sucesso"),
-                    @ApiResponse(responseCode = "400", description = "Valor inválido"),
-                    @ApiResponse(responseCode = "403", description = "Usuário não autenticado"),
-                    @ApiResponse(responseCode = "409", description = "Depósito não autorizado")
+                    @ApiResponse(responseCode = "201", description = "Depósito criado com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Response.class))),
+                    @ApiResponse(responseCode = "400", description = "Valor inválido", content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "401", description = "Usuário não autenticado", content = @Content),
+                    @ApiResponse(responseCode = "409", description = "Depósito não autorizado", content = @Content(mediaType = "application/json"))
             }
     )
     public ResponseEntity<Response> post(@RequestBody Request request) {
@@ -38,11 +42,13 @@ public class CreateDepositController {
         return ResponseEntity.status(HttpStatus.CREATED).body(Response.of(deposit));
     }
 
+    @Schema(name = "CreateDepositRequest")
     public record Request(
             @NotNull
             BigDecimal amount
     ) {}
 
+    @Schema(name = "CreateDepositResponse")
     public record Response(
             Long id,
             Long userId,
